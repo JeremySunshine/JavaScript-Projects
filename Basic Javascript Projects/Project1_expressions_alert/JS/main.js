838f6c2495c6ced866fe901d13f698b935d49892
Jeremy Sonnenschein <100240007+JeremySunshine@users.noreply.github.com>
2022-04-23 12:09:30 -0400

    Basic Java Assignment

---
+++ Basic Javascript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,12 @@
+var x = "Hey My name is Jeremy!";//Assigning my name to the varible x//
+window.alert(x);//Window that Alerts and says prints the varible//
+
+var Family = "The Sonnenscheins", Dad = "Jeremy", Mom = "Sykla",//variable thats has multiple varibles//
+    Son = "Sloan";
+document.write(Dad);//This is tellling it to write the Dad varible//
+
+document.write(4+4);//Writes out 4 plus 4 which equals 8//
+
+document.write("\"Be who you are and say what you feel."//using quottion marks//
++ "because those who mind don\'t matter and those who matter don\'t mind.\""
++ "- Dr. Suess");
