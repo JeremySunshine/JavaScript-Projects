fd23ea85f3b120183980a59881d879c2ad5d5fcd
Jeremy Sonnenschein <100240007+JeremySunshine@users.noreply.github.com>
2022-04-25 10:16:08 -0400

    Functions Assignment

---
+++ Basic Javascript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,5 @@
+function myFunction() { //This function allows the user to click on the "Click here" and it will show the message with the += operator//
+    var sentence = " I am Learning ";
+    sentence += "a lot from this book!";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
