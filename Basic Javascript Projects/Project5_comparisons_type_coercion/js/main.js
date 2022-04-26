e34e08038fdaaf05fe1db961417ee2e113342438
Jeremy Sonnenschein <100240007+JeremySunshine@users.noreply.github.com>
2022-04-26 11:21:46 -0400

    Project 5 Javascript

---
+++ Basic Javascript Projects/Project5_comparisons_type_coercion/js/main.js
@@ -0,0 +1,23 @@
+function my_Function() {
+    document.getElementById("test").innerHTML = 0/0; 
+}
+
+console.log(2 + 2);
+
+console.log(19 > 40);// shows false in console log //
+
+document.write(typeof "Word") ;
+
+function not_Funtion() {
+    document.getElementById("not").innerHTML = !(20 > 10 ); // Not Operator //
+}
+
+document.write( 5 > 2 && 10 > 4 ) ; 
+
+document.write( 5 > 10 || 10 > 4 ) ; // shows less than //
+
+X = 10; // makes 10 equal X // 
+Y = 100; // makes 100 equal Y//
+document.write(X === Y);
+
+document.write(3 == 22);// returns false // 
