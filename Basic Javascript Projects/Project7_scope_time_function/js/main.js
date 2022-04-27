082c4e3b7189299ef6c0818f9853860d213ebf65
Jeremy Sonnenschein <100240007+JeremySunshine@users.noreply.github.com>
2022-04-27 12:45:55 -0400

    Project7_Scope_Time_Function

---
+++ Basic Javascript Projects/Project7_scope_time_function/js/main.js
@@ -0,0 +1,50 @@
+var X = 10;
+function Add_numbers_1() {
+    document.write(20+ X + "<br>");
+}
+
+function Add_numbers_2() {
+    document.write( X + 100);
+}
+Add_numbers_1();
+Add_numbers_2();
+
+
+
+function Add_numbers_3() {
+    var Y = 20;
+    document.write(20 + Y + "<br>")
+}
+
+function Add_numbers_4() {
+    var Y = 20;
+    document.write(Y + 100);
+}
+Add_numbers_3();
+Add_numbers_4();
+
+
+function Clock_function() {
+    var Time = new Date().getHours();
+    var Reply;
+    if (Time < 12 == Time > 0) {
+        Reply = "It is morning time.";
+    }
+    else if (Time >= 12 == Time < 18) {
+        Reply = "it is the afternoon.";
+    }
+    else {
+        Reply = "It is evening time.";
+    }
+    document.getElementById("Time_of_day").innerHTML = Reply;
+}
+
+if (10 < 20) {
+    document.write(" The number 10 is less than the number 20 ");
+}
+
+function error_Function() {
+    var x = 10;
+    if (y > 12);
+    document.write("10 is less than 12");
+}
