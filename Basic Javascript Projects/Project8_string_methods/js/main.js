c6ab8de816c031698d705262ac422a86e2631425
Jeremy Sonnenschein <100240007+JeremySunshine@users.noreply.github.com>
2022-04-27 13:30:01 -0400

    Project8_Strring_Methods

---
+++ Basic Javascript Projects/Project8_string_methods/js/main.js
@@ -0,0 +1,24 @@
+function slice_Method() { // Slice method //
+    var Sentence = "All work and no play makes Johnny a dull boy.";
+    var Section= Sentence.slice(27,33);
+    document.getElementById("Slice").innerHTML = Section;
+}
+
+function string_Method() { // string method //
+    var X = 182;
+    document.getElementById("Numbers_to_string").innerHTML = X.toString();
+}
+
+function precision_Method() { // precision method //
+    var Y = 12938.3012987376112;
+    document.getElementById("Precision").innerHTML = Y.toPrecision(10);
+}
+
+function full_Sentence() { // concat method //
+    var part_1 = " I have ";
+    var part_2 = "made this ";
+    var part_3 = "into a complete ";
+    var part_4 = "sentence.";
+    var whole_sentence = part_1.concat(part_2, part_3, part_4);
+    document.getElementById("Concatenate").innerHTML = whole_sentence;
+}
