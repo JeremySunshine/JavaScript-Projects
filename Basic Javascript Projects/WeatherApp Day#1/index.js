a0c3badd2be8377bf7fb6cb2c7fa533a2664afa4
Jeremy Sonnenschein <100240007+JeremySunshine@users.noreply.github.com>
2023-03-07 09:46:33 -0500

    update

--- Basic Javascript Projects/WeatherApp Day#1/index.js
+++ Basic Javascript Projects/WeatherApp Day#1/index.js
@@ -28,7 +28,7 @@ search.addEventListener('click', () =>{
 
     const image = document.querySelector('.weather-box img');
     const temperature = document.querySelector('.weather-box .temperature');
-    const description = document.querySelector('.weather-box . description');
+    const description = document.querySelector('.weather-box .description');
     const humidity = document.querySelector('weather-details .humidity span');
     const hwind = document.querySelector('weather-details .wind span');
 
